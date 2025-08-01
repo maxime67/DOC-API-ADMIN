98970a19bdea20ae307d1d90e5395ae3bd69e7d9
Maxim <maxime.heim3@gmail.com>
2025-08-01 18:35:19 +0200

    add key + cert for ssl

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const https = require('https');
+const fs = require('fs');
+const path = require('path');
 const connectDB = require('./config/db');
 const categoryRoutes = require('./routes/categoryRoutes');
 const documentationRoutes = require('./routes/documentationRoutes');
 require('dotenv').config();
 const cors = require('cors');
+
 const app = express();
 
 // Connect to MongoDB
@@ -11,12 +15,22 @@ connectDB();
 
 // Middleware
 app.use(express.json());
-app.use(cors())
+app.use(cors());
+
 // Routes
 app.use('/api/categories', categoryRoutes);
 app.use('/api/documentation', documentationRoutes);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+const HTTPS_PORT = process.env.HTTPS_PORT || 3443;
+
+// Configuration HTTPS
+const httpsOptions = {
+    cert: fs.readFileSync('/etc/letsencrypt/live/k0li.fr/fullchain.pem'),
+    key: fs.readFileSync('/etc/letsencrypt/live/k0li.fr/privkey.pem')
+};
+
+// Démarrage du serveur HTTPS
+https.createServer(httpsOptions, app).listen(HTTPS_PORT, () => {
+    console.log(`HTTPS Server is running on port ${HTTPS_PORT}`);
 });
